9ab8a006b9ea1dc7a1ede4e30060526e55c0a9d4
Permishin <ermishinpavel@gmail.com>
2021-02-15 20:49:50 +0300

    USER PreAuthorize

--- src/main/java/com/example/productcatalog/controller/UserController.java
+++ src/main/java/com/example/productcatalog/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
+@PreAuthorize("hasAuthority('USER')")
 public class UserController {
 
     private final UserRepo userRepo;
